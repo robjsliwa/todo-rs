152e6f7523b5d8158dcfc2614f18dd737e243147
Rob Sliwa <robjsliwa@gmail.com>
2023-09-22 21:26:52 -0400

    Use MongoStore instead of MemStore.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use crate::auth::with_jwt;
 use crate::routes::router;
 use crate::storage::{store::TodoStore, MongoStore};
-use log::info;
+use log::{error, info};
 use std::env;
 use std::net::SocketAddr;
 use std::sync::Arc;
@@ -14,16 +14,16 @@ mod storage;
 
 #[derive(Debug)]
 struct Config {
-    memstore_path: String,
     jwt_secret: String,
     server_addr: SocketAddr,
+    mongo_uri: String,
 }
 
 impl Config {
     fn from_env() -> Result<Self, env::VarError> {
         const DEFAULT_ADDR: &str = "0.0.0.0";
         const DEFAULT_PORT: &str = "3030";
-        let memstore_path = env::var("MEMSTORE_PATH")?;
+        let mongo_uri = env::var("MONGO_URI")?;
         let jwt_secret = env::var("JWT_SECRET")?;
         let ip_address = env::var("TODO_ADDR")
             .map(|s| {
@@ -47,9 +47,9 @@ impl Config {
         let server_addr = full_addr.parse().map_err(|_| env::VarError::NotPresent)?;
 
         Ok(Self {
-            memstore_path,
             jwt_secret,
             server_addr,
+            mongo_uri,
         })
     }
 }
@@ -60,9 +60,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let config = Config::from_env().expect("Failed to load configuration");
 
-    let mongo_store = MongoStore::init(config.memstore_path)
+    let mongo_store = MongoStore::init(config.mongo_uri)
         .await
-        .unwrap_or_else(|e| panic!("Failed to connect to MongoDB: {:?}", e));
+        .unwrap_or_else(|e| {
+            error!("Failed to connect to MongoDB: {:?}", e);
+            std::process::exit(1);
+        });
     let store: Arc<dyn TodoStore> = Arc::new(mongo_store.clone());
     let store_for_routes = store.clone();
     info!("Server started at {}", config.server_addr);
