86fd9a1a3f27ea50a83759bedd7f22508e70fda5
Rob Sliwa <robjsliwa@gmail.com>
2023-11-09 14:25:24 +0100

    Added identity service and created multi Tiltfile dev environment.

---
+++ cli/src/auth/mod.rs
@@ -0,0 +1,7 @@
+pub mod get_token;
+pub mod login;
+pub mod token_response;
+
+pub use get_token::*;
+pub use login::*;
+pub use token_response::*;
---
+++ cli/src/main.rs
@@ -0,0 +1,24 @@
+use crate::commands::{invoke_command, CommandContext};
+use crate::config::Config;
+use cred_store::{CredStore, Credentials};
+
+mod auth;
+mod commands;
+mod config;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config = Config::from_env()?;
+    let mut credentials = Credentials::new()
+        .set_file_name(".credentials".to_string())
+        .build()
+        .load()?;
+
+    let mut context = CommandContext {
+        config: &config,
+        cred_store: &mut credentials,
+    };
+
+    invoke_command(&mut context);
+
+    Ok(())
+}
